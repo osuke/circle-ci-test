5d6bc8ca0173217e7c4974a768aec96ab50f2fb8
osuke <funxion.mail@gmail.com>
2020-07-08 00:51:46 +0900

    update

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,5 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest",
-  reporters: [
-    'default',
-    [
-      'jest-junit',
-      {
-        suiteName: 'jest tests',
-        outputDirectory: 'reports'
-      },
-    ],
-  ],
   collectCoverageFrom: [
     "src/**/*.{js|vue}"
   ]
