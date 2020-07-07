01117422a442a94d340d05abc9b8f54323301f5a
osuke <funxion.mail@gmail.com>
2020-07-08 01:34:59 +0900

    update

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest",
   testMatch: [
-    "tests/unit/**/*.spec.js",
+    "<rootDir>/tests/unit/**/*.spec.js",
   ],
   collectCoverageFrom: [
     "src/**/*.{js|vue}"
